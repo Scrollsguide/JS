b8a7da5b46e9360a341bd399ff3884d5ab120109
Koen Basten <kbasten@gmail.com>
2012-06-22 20:31:03 +0200

    init

---
+++ slider.js
@@ -0,0 +1,41 @@
+var slideTimeOut = 8000; // delay between slide changes
+var slideTime = 500; // animation time
+var slideWidth; // width of a single slide in px
+var slideId;
+
+var curSlide = 0;
+var numSlides;
+
+var direction = 1; // 1 = right, -1 = left, init direction
+
+var slideTimer;
+
+$(document).ready(function(){
+	slideId = $("#slidercontent");
+	
+	slideWidth = slideId.children().eq(0).width();
+	numSlides = slideId.children().length;
+	slideId.css("width", (numSlides * slideWidth) + "px");
+	slideTimer = setTimeout('nextSlide()', slideTimeOut);
+});
+
+function nextSlide(){	
+	
+	if (curSlide >= numSlides - 1){
+		direction = -1; // reverse direction
+	}
+	if (curSlide <= 0){
+		direction = 1; // reverse direction
+	}
+	
+	curSlide+= direction;
+	toSlide(curSlide);	
+	
+	slideTimer = setTimeout('nextSlide()', slideTimeOut);
+	
+}
+
+function toSlide(nr){
+	var newMargin = slideWidth * nr; 
+	slideId.animate({ "margin-left":  "-" + newMargin + "px" }, slideTime);
+}
